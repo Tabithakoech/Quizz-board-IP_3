933a8bb37aaeb4cfe6f51b685dc6f970a9835761
TabithaKoech <tabbyk1718@gmail.com>
2022-05-15 17:23:57 +0300

    added if else if statements for different student grades and recomendations

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,13 +8,46 @@ function checkScore(){
     var q4=document.quizz.question4.value;
     var q5=document.quizz.question5.value;
 
+    var yourScore=document.getElementById("yourScore");
+
+    var quizz=document.getElementById("quizz");
+
     //checking for correct answers and incrementing the marks
-    if (q1=="js"){marks+=20}
-    if (q2=="int"){marks+=20}
-    if (q3=="Both of the above"){marks+=20}
-    if (q4=="length()"){marks+=20}
-    if (q5=="push()"){marks+=20}
+    if (q1=="js"){
+        marks+=20
+    }
+    if (q2=="int"){
+        marks+=20
+    }
+    if (q3=="Both of the above"){
+        marks+=20
+    }
+    if (q4=="length()"){
+        marks+=20
+    }
+    if (q5=="push()"){
+        marks+=20
+    }
+    quizz.style.display='none';
+
+
+
     
-    document.write(marks);
+
+
+    if (marks>80){
+        document.getElementById("yourScore").innerHTML="You scored" + " " + marks +"%" + "You have execellently passed congratulations";
+
+    }
+    else if (marks>=50 && marks<80){
+        document.getElementById("yourScore").innerHTML="You scored" + " " + marks +"%" + "You have fairly passed ";
+        
+
+    
+    }
+    else{
+        document.getElementById("yourScore").innerHTML="You scored" + " " + marks +"%" + "You have scored poorly.Retake test ";
+    }
+    
     
 }
